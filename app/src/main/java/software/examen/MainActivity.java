ccbe53a39bb14f152ff05ae40dd32752b6038a6d
DarlynYami05 <dwverling@gmail.com>
2021-03-11 16:44:27 -0500

    Segundo commit

--- app/src/main/java/software/examen/MainActivity.java
+++ app/src/main/java/software/examen/MainActivity.java
@@ -4,11 +4,30 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import software.examen.WebService.Asynchtask;
+import software.examen.WebService.WebService;
+
+public class MainActivity extends AppCompatActivity implements Asynchtask {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Map<String, String> datos = new HashMap<String, String>();
+        WebService ws = new WebService("https://revistas.uteq.edu.ec/ws/journals.php",
+                datos, this, this);
+        ws.execute("GET");
+    }
+
+    @Override
+    public void processFinish(String result) throws JSONException {
+
     }
 }
